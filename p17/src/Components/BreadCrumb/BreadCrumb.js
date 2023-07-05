84adcdc5796f48eff35c3ff2e54511f868fae6d7
mohammadreza <spd.mohammadreza@gmail.com>
2023-07-05 07:13:33 -0700

    sabzlearn-courseInfo-breadcrumb-ui

---
+++ p17/src/Components/BreadCrumb/BreadCrumb.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function BreadCrumb({ links }) {
+    return (
+        <div className='px-32'>
+            <div className='flex text-xs  items-center gap-2 bg-gray-200 my-5 p-3 rounded-md '>
+                <div className='bg-white-color w-7 h-7 flex justify-center items-center rounded-md text-gray-500'>
+                    <i class="fa-solid fa-house"></i>
+                </div>
+                {
+                    links.map(link => (
+                        <div className='flex items-center gap-1'>
+                            <a href={link.to}>{link.title}</a>
+                            {
+                                link.id !== links.length ? (
+                                    <i class="fa-solid fa-chevron-left"></i>
+                            ) : (
+                                null
+                            )
+                            }
+                        </div>
+                    ))
+                }
+
+            </div>
+        </div>
+    )
+}
--- p17/src/pages/CourseInfo/CourseInfo.js
+++ p17/src/pages/CourseInfo/CourseInfo.js
@@ -2,6 +2,7 @@ import React from 'react'
 import TopBar from '../../Components/TopBar/TopBar'
 import NavBar from '../../Components/NavBar/NavBar'
 import Footer from '../../Components/Footer/Footer'
+import BreadCrumb from '../../Components/BreadCrumb/BreadCrumb'
 
 export default function CourseInfo() {
   return (
@@ -9,6 +10,12 @@ export default function CourseInfo() {
       <div>
         <TopBar></TopBar>
         <NavBar></NavBar>
+
+        <BreadCrumb links={[
+          {id:1, title: 'خانه', to:'/'},
+          {id:2, title:'آموزش برنامهع نویسی فرانت اند', to:'/category-info/frontend'},
+          {id:3, title:'دوره متخصص جاوا اسکریپت', to:'course-info/js-expert'}
+        ]}></BreadCrumb>
 
         <Footer></Footer>
       </div>
